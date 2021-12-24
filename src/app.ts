e2fc31f3c82242d80fbfb5d89c11608618a3e331
Omar-Belghaouti <omarbelghaouti@gmail.com>
2021-12-24 21:01:54 +0100

    arrays & tuples

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,25 @@
 const person: {
   name: string;
   age: number;
+  hobbies: string[];
+  role: [number, string];
 } = {
   name: "Omar",
   age: 23,
+  hobbies: ["Sports", "Cooking"],
+  role: [2, "author"], // array of numbers and strings
 };
 
-console.log(person);
+// person.role.push("admin"); // add new element to the array
+// person.role[1] = 10; // change the value of the second element
+
+// person.role = [0, "admin", "user"];
+
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
+console.log(person.name);
+
+for (const hobby in person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
