0a8d681553f2e85270f1c691d63e85e45be01a39
Omar-Belghaouti <omarbelghaouti@gmail.com>
2021-12-24 22:45:27 +0100

    never type

--- src/app.ts
+++ src/app.ts
@@ -6,3 +6,14 @@ userInput = "Max";
 if (typeof userInput === "string") {
   userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+  throw {
+    message,
+    errorCode: code,
+  };
+  // while (true) {}
+}
+
+const result = generateError("An error occured!", 500);
+console.log(result);
