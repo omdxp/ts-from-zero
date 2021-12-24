6e33e2d51e12fa2b29bd0a149ee17fd2009231b8
Omar-Belghaouti <omarbelghaouti@gmail.com>
2021-12-24 22:33:17 +0100

    callback function types

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,11 @@ function printResult(num: number) {
   console.log("Result: " + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(5, 12));
 
 // let combineValues: Function;
@@ -16,3 +21,8 @@ combineValues = add;
 // combineValues = printResult;
 
 console.log(combineValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+  return result;
+});
