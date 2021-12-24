d5047a476d5c17bfdb12c938697b740960b732d3
Omar-Belghaouti <omarbelghaouti@gmail.com>
2021-12-24 20:48:35 +0100

    object types

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,9 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
-}
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: "Omar",
+  age: 23,
+};
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = "Result is: ";
-
-add(number1, number2, printResult, resultPhrase);
+console.log(person);
