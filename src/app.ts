92a55317a4da5f26908d3e3c61581940582e5cb2
Omar-Belghaouti <omarbelghaouti@gmail.com>
2021-12-24 22:03:35 +0100

    function return types

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,14 @@
-type Combinable = number | string;
-type ConversionDescriptor = "as-number" | "as-text";
-
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: ConversionDescriptor
-) {
-  let result;
-  if (
-    (typeof input1 === "number" && typeof input2 === "number") ||
-    resultConversion === "as-number"
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
-  // if (resultConversion === "as-number") {
-  //   return +result;
-  // } else {
-  //   return result.toString();
-  // }
+function add(n1: number, n2: number): number {
+  return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, "as-number");
-console.log(combinedAges);
+function printResult(num: number): void {
+  console.log("Result: " + num);
+}
 
-const combinedNames = combine("Max", "Anna", "as-text");
-console.log(combinedNames);
+function printResult2(num: number): undefined {
+  console.log("Result: " + num);
+  return;
+}
+
+printResult(add(5, 12));
