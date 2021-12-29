2b911bb330791597ff48c8b7102214b3f25dc6c2
Omar-Belghaouti <omarbelghaouti@gmail.com>
2021-12-29 10:43:52 +0100

    using watch mode and tsconfig

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,2 @@
-let userInput: unknown;
-let userName: string;
-
-userInput = 5;
-userInput = "Max";
-if (typeof userInput === "string") {
-  userName = userInput;
-}
-
-function generateError(message: string, code: number): never {
-  throw {
-    message,
-    errorCode: code,
-  };
-  // while (true) {}
-}
-
-const result = generateError("An error occured!", 500);
-console.log(result);
+console.log("Hello World");
+let x: number = 10;
