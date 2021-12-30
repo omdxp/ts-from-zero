4fa485d6f50c7f04a360171dd7ecc7b367a3c921
Omar-Belghaouti <omarbelghaouti@gmail.com>
2021-12-30 17:54:36 +0100

    next js in ts

--- src/analytics.ts
+++
@@ -1,8 +0,0 @@
-function sendAnalytics(data: any) {
-  let userName = "Omar";
-  console.log("data");
-}
-sendAnalytics({
-  event: "clicked",
-  time: new Date().toISOString(),
-});
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,49 @@
-const button = document.querySelector("button")!;
+const userName = "Omar";
+// userName = "Max";
+let age = 30;
+age = 29;
 
-// a comment
-button.addEventListener("click", () => {
-  console.log("clicked");
-});
+// let result;
+
+// function add(a: number, b: number) {
+//   result = a + b;
+//   return result;
+// }
+
+// if (age > 20) {
+//   let isOld = true;
+// }
+
+// const add = (a: number, b: number = 5) => a + b;
+
+// const printOutput: (a: number | string) => void = (output) =>
+//   console.log(output);
+// printOutput(add(2));
+
+const hobbies = ["Sports", "Cooking"];
+
+const activeHobbies = ["Hiking", ...hobbies];
+
+activeHobbies.push(...hobbies);
+
+const person = {
+  name: "Omar",
+  age: 23,
+};
+
+const copiedPerson = { ...person };
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((prev, curr) => prev + curr, 0);
+};
+
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+
+console.log(hobbies, hobby1, hobby2);
+
+const { name: firstName, age: ageVal } = person;
+
+console.log(firstName, ageVal);
