56dc625020453f11f3681703d6cfff45ff02445e
Omar-Belghaouti <omarbelghaouti@gmail.com>
2021-12-30 22:08:06 +0100

    getters and setters

--- src/app.ts
+++ src/app.ts
@@ -29,9 +29,26 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found.");
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please pass in a valid value!");
+    }
+    this.addReport(value);
+  }
+
   constructor(id: string, private reports: string[]) {
     super(id, "Accounting");
     this.reports = reports;
+    this.lastReport = reports[0];
   }
 
   addEmployee(employee: string): void {
@@ -43,6 +60,7 @@ class AccountingDepartment extends Department {
 
   addReport(text: string) {
     this.reports.push(text);
+    this.lastReport = text;
   }
 
   printReports() {
@@ -64,7 +82,11 @@ console.log(it);
 
 const accounting = new AccountingDepartment("d2", []);
 
+accounting.mostRecentReport = "Your End Report";
+
 accounting.addReport("Something went wrong...");
+
+console.log(accounting.mostRecentReport);
 
 accounting.addEmployee("Manu");
 
