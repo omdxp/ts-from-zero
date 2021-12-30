6a93d21c11fc0fc5ac3ffcecd6867cb25eb57710
Omar-Belghaouti <omarbelghaouti@gmail.com>
2021-12-30 21:02:29 +0100

    shorthand initialization

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 class Department {
-  name: string; // default is public
+  // private id: string;
+  // private name: string;
   private employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(private id: string, public name: string) {
+    // this.name = n;
   }
 
   describe(this: Department) {
-    console.log("Department: " + this.name);
+    console.log(`Department (${this.id}): ${this.name}`);
   }
 
   addEmployee(employee: string) {
@@ -20,7 +21,7 @@ class Department {
   }
 }
 
-const accounting = new Department("Accounting");
+const accounting = new Department("d1", "Accounting");
 
 accounting.addEmployee("Omar");
 accounting.addEmployee("Max");
