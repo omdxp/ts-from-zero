72fcec8b3b770736befe46c4262001a0008cc6fb
Omar-Belghaouti <omarbelghaouti@gmail.com>
2021-12-30 20:40:23 +0100

    constructor, functions and this keyword

--- src/app.ts
+++ src/app.ts
@@ -1,49 +1,19 @@
-const userName = "Omar";
-// userName = "Max";
-let age = 30;
-age = 29;
+class Department {
+  name: string;
 
-// let result;
+  constructor(n: string) {
+    this.name = n;
+  }
 
-// function add(a: number, b: number) {
-//   result = a + b;
-//   return result;
-// }
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
+}
 
-// if (age > 20) {
-//   let isOld = true;
-// }
+const accounting = new Department("Accounting");
 
-// const add = (a: number, b: number = 5) => a + b;
+accounting.describe();
 
-// const printOutput: (a: number | string) => void = (output) =>
-//   console.log(output);
-// printOutput(add(2));
+const accountingCopy = { name: "DUMMY", describe: accounting.describe };
 
-const hobbies = ["Sports", "Cooking"];
-
-const activeHobbies = ["Hiking", ...hobbies];
-
-activeHobbies.push(...hobbies);
-
-const person = {
-  name: "Omar",
-  age: 23,
-};
-
-const copiedPerson = { ...person };
-
-const add = (...numbers: number[]) => {
-  return numbers.reduce((prev, curr) => prev + curr, 0);
-};
-
-const addedNumbers = add(5, 10, 2, 3.7);
-console.log(addedNumbers);
-
-const [hobby1, hobby2, ...remainingHobbies] = hobbies;
-
-console.log(hobbies, hobby1, hobby2);
-
-const { name: firstName, age: ageVal } = person;
-
-console.log(firstName, ageVal);
+accountingCopy.describe();
