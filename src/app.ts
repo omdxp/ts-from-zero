a59042fff86216fece5e93fa2bf99247e6673452
Omar-Belghaouti <omarbelghaouti@gmail.com>
2021-12-30 20:56:43 +0100

    public and private access modifiers

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-  name: string;
+  name: string; // default is public
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -8,12 +9,27 @@ class Department {
   describe(this: Department) {
     console.log("Department: " + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department("Accounting");
 
+accounting.addEmployee("Omar");
+accounting.addEmployee("Max");
+
+// accounting.employees[2] = "Anna";
+
 accounting.describe();
+accounting.printEmployeeInformation();
 
-const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+// const accountingCopy = { name: "DUMMY", describe: accounting.describe };
 
-accountingCopy.describe();
+// accountingCopy.describe();
