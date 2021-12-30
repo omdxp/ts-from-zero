aaeec588c6ccb5bd5a84acabd7d56a0617e35593
Omar-Belghaouti <omarbelghaouti@gmail.com>
2021-12-30 22:13:42 +0100

    static methods and properties

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 class Department {
+  static fiscalYear = 2021;
   // private readonly id: string;
   // private name: string;
   protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.name = n;
+    // console.log(Department.fiscalYear);
+  }
+
+  static createEmployee(name: string) {
+    return {
+      name: name,
+    };
   }
 
   describe(this: Department) {
@@ -67,6 +75,9 @@ class AccountingDepartment extends Department {
     console.log(this.reports);
   }
 }
+
+const employee1 = Department.createEmployee("Max");
+console.log(employee1, Department.fiscalYear);
 
 const it = new ITDepartment("d1", ["Omar"]);
 
