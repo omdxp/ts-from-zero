d4440a5f9150af22802266df4b2938cd500d9c91
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-05 10:52:18 +0100

    type guards

--- src/app.ts
+++ src/app.ts
@@ -32,3 +32,62 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInfo(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("Start date: " + emp.startDate);
+  }
+}
+
+printEmployeeInfo(e1);
+printEmployeeInfo({
+  name: "Omar",
+  startDate: new Date(),
+});
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving...");
+  }
+
+  loadCargo(amount: number) {
+    console.log("Loading cargo..." + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  // if ("loadCargo" in vehicle) {
+  //   vehicle.loadCargo(1500);
+  // }
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1500);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
