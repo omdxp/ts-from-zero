7e92110b07e4ab6fe9c97b24e2e96d540ac9754e
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-05 21:11:01 +0100

    generic function

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
-const names: Array<string> = ["Omar", "Yasser"]; // string[]
-names[0].split(" ");
+// const names: Array<string> = ["Omar", "Yasser"]; // string[]
+// names[0].split(" ");
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("This is done!");
-  }, 2000);
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("This is done!");
+//   }, 2000);
+// });
 
-promise.then((data) => {
-  data.split(" ");
-});
+// promise.then((data) => {
+//   data.split(" ");
+// });
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "Omar" }, { age: 23 });
+console.log(mergedObj.age);
