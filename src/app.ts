f1309f1ff02ba05ee4ee9842303f12af62d8b612
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-05 21:43:02 +0100

    generic classes

--- src/app.ts
+++ src/app.ts
@@ -42,3 +42,38 @@ function extractAndConvert<T extends object, U extends keyof T>(
 }
 
 console.log(extractAndConvert({ name: "Omar" }, "name"));
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Omar");
+textStorage.addItem("Yasser");
+textStorage.removeItem("Omar");
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(23);
+
+// const objStorage = new DataStorage<object>();
+// const omarObj = { name: "Omar" };
+// objStorage.addItem(omarObj);
+// objStorage.addItem({ name: "Yasser" });
+// objStorage.removeItem(omarObj);
+// console.log(objStorage.getItems());
