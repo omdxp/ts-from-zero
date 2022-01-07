454a0a408f5162d058202d8112735a6ba094ccf3
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-07 11:07:44 +0100

    returning a class in class decorator

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,33 @@
-// function Logger(logString: string) {
-//   console.log("LOGGER FACTORY");
-//   return function (constructor: Function) {
-//     console.log(logString);
-//     console.log(constructor);
-//   };
-// }
+function Logger(logString: string) {
+  console.log("LOGGER FACTORY");
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
+}
 
-// function WithTemplate(template: string, hookId: string) {
-//   console.log("TEMPLATE FACTORY");
-//   return function (constructor: any) {
-//     console.log("Rendering template");
-// const hookEl = document.getElementById(hookId);
-// const p = new constructor();
-// if (hookEl) {
-//   hookEl.innerHTML = template;
-//   hookEl.querySelector("h1")!.textContent = p.name;
-// }
-//   };
-// }
+// this will execute when instance of class is created
+function WithTemplate(template: string, hookId: string) {
+  console.log("TEMPLATE FACTORY");
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    originalConstructor: T
+  ) {
+    return class extends originalConstructor {
+      constructor(..._: any[]) {
+        super();
+        console.log("Rendering template");
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector("h1")!.textContent = this.name;
+        }
+      }
+    };
+  };
+}
 
-// @Logger("LOGGING - PERSON")
-// @WithTemplate("<h1>My Person Object</h1>", "app")
+@Logger("LOGGING - PERSON")
+@WithTemplate("<h1>My Person Object</h1>", "app")
 class Person {
   name = "Omar";
 
