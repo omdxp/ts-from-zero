a3457e7061c94cd21e6e3f9b91b2477d18f1c2e8
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-07 10:45:36 +0100

    property decorator

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,26 @@
-function Logger(logString: string) {
-  console.log("LOGGER FACTORY");
-  return function (constructor: Function) {
-    console.log(logString);
-    console.log(constructor);
-  };
-}
+// function Logger(logString: string) {
+//   console.log("LOGGER FACTORY");
+//   return function (constructor: Function) {
+//     console.log(logString);
+//     console.log(constructor);
+//   };
+// }
 
-function WithTemplate(template: string, hookId: string) {
-  console.log("TEMPLATE FACTORY");
-  return function (constructor: any) {
-    console.log("Rendering template");
-    const hookEl = document.getElementById(hookId);
-    const p = new constructor();
-    if (hookEl) {
-      hookEl.innerHTML = template;
-      hookEl.querySelector("h1")!.textContent = p.name;
-    }
-  };
-}
+// function WithTemplate(template: string, hookId: string) {
+//   console.log("TEMPLATE FACTORY");
+//   return function (constructor: any) {
+//     console.log("Rendering template");
+// const hookEl = document.getElementById(hookId);
+// const p = new constructor();
+// if (hookEl) {
+//   hookEl.innerHTML = template;
+//   hookEl.querySelector("h1")!.textContent = p.name;
+// }
+//   };
+// }
 
-@Logger("LOGGING - PERSON")
-@WithTemplate("<h1>My Person Object</h1>", "app")
+// @Logger("LOGGING - PERSON")
+// @WithTemplate("<h1>My Person Object</h1>", "app")
 class Person {
   name = "Omar";
 
@@ -32,3 +32,33 @@ class Person {
 const pers = new Person();
 
 console.log(pers);
+
+// ---
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator!");
+  console.log(target, propertyName);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price - should be positive!");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
